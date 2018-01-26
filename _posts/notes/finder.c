3abd674723e40ae5e05f041c2877c9f00cf8dc28
Spencer Tiberi <spencertiberi@gmail.com>
2018-01-26 09:03:39 -0500

    add whodunit

---
+++ _posts/notes/finder.c
@@ -0,0 +1,31 @@
+// recursive function to iterate through directories and search files
+int seek(directory dir)
+{
+    // populate directory
+
+    // iterate over the paths in the directory dir
+
+    // check is type of current path is a file
+
+    // create a file pointer to read contents of the file
+
+    // create a buffer (array of set size) of characters to check for key
+
+    // populate the buffer
+
+    // while not the end of the file, look for key in buffer
+
+    // if the string has been found via strstr()
+
+    // create a FILE pointer to the outfile found.txt
+
+    // write name of file to found.txt
+
+    // close file pointers
+
+    // else, must be a directory so create a new directory called subdir
+
+    // search contents of subdir
+
+    return -1;
+}
